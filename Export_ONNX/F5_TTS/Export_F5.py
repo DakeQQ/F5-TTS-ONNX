e3ca7ec1b5fcad3c60293fd4aff39ad67d7288a5
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-01 10:18:08 +0800

    Update Export_F5.py

--- Export_ONNX/F5_TTS/Export_F5.py
+++ Export_ONNX/F5_TTS/Export_F5.py
@@ -273,6 +273,12 @@ def list_str_to_idx(
     return text
 
 
+def normalize_to_int16(audio):
+    max_val = np.max(np.abs(audio.astype(np.float32)))
+    scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
+    return (audio * float(scaling_factor)).astype(np.int16)
+    
+
 print("\n\nStart to Export the F5-TTS Preprocess Part.")
 with torch.inference_mode():
     # Dummy for Export the F5_Preprocess part
@@ -465,7 +471,8 @@ out_name_C0 = out_name_C[0].name
 
 
 # Run F5-TTS by ONNX Runtime
-audio = np.array(AudioSegment.from_file(reference_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int16)
+audio = np.array(AudioSegment.from_file(reference_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int32)
+audio = normalize_to_int16(audio)
 audio_len = len(audio)
 audio = audio.reshape(1, 1, -1)
 
